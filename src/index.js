f561543a3faf1d1b14a079643b82d0c8fe2564d0
Omar Cardoze <cardozeomar@gmail.com>
2022-07-29 14:29:48 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const root = document.getElementById('root')
+
+const days = [
+    {
+        title: 'Comienzo el Lunes 1 de Agosto',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, pariatur.'
+    },
+]
+
+days.map(({ title, description }, index ) => {
+
+    let day = index + 1
+
+    root.innerHTML += `
+         <div class="project-card">
+            <div class="card-title">
+                <a class="title" href="/days/day0${day}/day0${day}.html">${title}</a>
+                <span class="text-white day-challenge">${day}</span>
+            </div>
+            <p class="card-description">${description}</p>
+        </div>
+    `
+})
